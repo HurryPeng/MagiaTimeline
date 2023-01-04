5dabc00b60387e9f90adfda84bfb4af8c9668865
Haoran Peng <hurrypeng@mail.ustc.edu.cn>
2023-01-04 21:12:19 +0800

    Adaptive ROI

--- MagiaTimeline.py
+++ MagiaTimeline.py
@@ -1,15 +1,15 @@
-# import typing
 import cv2 as cv
-# import pytesseract
-# import easyocr
 
-# import manga_ocr
-# mocr = manga_ocr.MangaOcr()
-# reader = easyocr.Reader(['ja','en']) 
+dialogueBoarderUp    = 0.7264
+dialogueBoarderDown  = 0.8784
+dialogueBoarderLeft  = 0.3125
+dialogueBoarderRight = 0.6797
 
 def main():
     src = cv.VideoCapture("sample.mp4")
-    size = (1280, 592)
+    width = int(src.get(cv.CAP_PROP_FRAME_WIDTH))
+    height = int(src.get(cv.CAP_PROP_FRAME_HEIGHT))
+    size = (width, height)
     dst = cv.VideoWriter('out.mp4', cv.VideoWriter_fourcc('m','p','4','v'), 29, size)
     
     frameCount = 0
@@ -21,15 +21,15 @@ def main():
             break
         
         # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        poi = frame[420:530, 400:870]
-        poiGray = cv.cvtColor(poi, cv.COLOR_BGR2GRAY)
+        roi = frame[int(height * dialogueBoarderUp) : int(height * dialogueBoarderDown), int(width * dialogueBoarderLeft) : int(width * dialogueBoarderRight)]
+        poiGray = cv.cvtColor(roi, cv.COLOR_BGR2GRAY)
         meanGray: float = cv.mean(poiGray)[0]
         _, poiBin = cv.threshold(poiGray, 192, 255, cv.THRESH_BINARY)
         meanBin: float = cv.mean(poiBin)[0]
 
-        isDialogue = meanGray > 200
-        hasText = meanBin < 254
-        isValidSubtitle = isDialogue and hasText
+        isWhiteBackground = meanGray > 200
+        hasText = meanBin < 254 and meanBin > 200
+        isValidSubtitle = isWhiteBackground and hasText
 
         print(frameCount, meanGray, meanBin, isValidSubtitle)
 
@@ -40,12 +40,14 @@ def main():
         # print(reader.readtext(frame))
         # print(mocr(framePil))
 
-        frameOut = None
+        frameOut = frame
         if isValidSubtitle:
-            frameOut = cv.putText(frame, "VALID SUBTITLE", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 0), 2)
-        else:
-            frameOut = frame
-        cv.imshow("show", poiBin)
+            frameOut = cv.putText(frameOut, "VALID SUBTITLE", (50, 50), cv.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3)
+        if isWhiteBackground:
+            frameOut = cv.putText(frameOut, "is white bg", (50, 75), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+        if hasText:
+            frameOut = cv.putText(frameOut, "has text", (50, 100), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+        cv.imshow("show", frameOut)
         if cv.waitKey(1) == ord('q'):
             break
         dst.write(frameOut)
