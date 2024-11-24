b31a58591a534020ab10e068dbe9fb3a230f2d97
HurryPeng <hrpeng@cs.washington.edu>
2024-11-23 20:08:00 -0800

    proj: add installing PyInstaller to build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo off
 python -V -V
 call venv\Scripts\activate.bat
+python -m pip install --upgrade pip
+python -m pip install pyinstaller
 pyinstaller MagiaTimeline.spec --noconfirm
 xcopy dist\MagiaTimeline\_internal\move_to_root\* dist\MagiaTimeline /E /H /K /Y
 rmdir /S /Q dist\MagiaTimeline\_internal\move_to_root
